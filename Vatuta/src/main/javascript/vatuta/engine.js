8a1e441634d5709499730def158ec7947272bf08
JuanFran Adame <juanfran.adame@gmail.com>
2015-09-16 00:30:39 +0200

    * JsDoc3 tuning

--- Vatuta/src/main/javascript/vatuta/engine.js
+++ Vatuta/src/main/javascript/vatuta/engine.js
@@ -1,10 +1,16 @@
+/**
+ * @module Engine
+ */
 define([ "dojo/_base/declare", "./vatuta/project.js", "underscorejs" ],
 	function(declare, Project, _) {
-		/** @class Engine */
+		/**
+	     * @constructor
+	     * @alias module:Engine
+	     */
 		return {
 			/** Calculate early start and late end dates of every task of the project
 			 * @function
-			 * @param {Project} Project to be calculated */
+			 * @param {Project} project Project to be calculated */
 			calculateEarlyStartLateEnding : function(project) {
 				return "Dojo";
 			}
--- Vatuta/src/main/javascript/vatuta/project.js
+++ Vatuta/src/main/javascript/vatuta/project.js
@@ -1,9 +1,22 @@
+/**
+ * @module Project
+ */
 define([ "dojo/_base/declare", "dojo/_base/lang", "./vatuta/task.js", "./vatuta/engine.js" ],
 		function(declare, lang, Task, Engine) {
+			/**
+		     * @exports Project
+		     */
 			return declare(null, {
+				/**
+				 * @constructs Project
+				 */
 				constructor : function(/* Object */kwArgs) {
 					lang.mixin(this, kwArgs);
 				},
+				/**
+				 * @function
+				 * @memberof Project
+				 */
 				getHello : function() {
 					this.task = new Task({
 						duration : 3
