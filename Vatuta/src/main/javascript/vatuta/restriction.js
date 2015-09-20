86c8827aeba7d8c3631a97200290a4dfaf4ac3e2
JuanFran Adame <juanfran.adame@gmail.com>
2015-09-20 23:36:29 +0200

    * Restriction changes

--- Vatuta/src/main/javascript/vatuta/restriction.js
+++ Vatuta/src/main/javascript/vatuta/restriction.js
@@ -18,6 +18,11 @@ define([ "dojo/_base/declare", "dojo/_base/lang" ], function(declare, lang) {
      * @exports Restriction
      */
 	var EndToStartDependency = declare("Vatuta.EndToStartDependency", Restriction, {
-		
+		getEndingTask: function() {
+			return this._endingTask;
+		},
+		getStartingTask: function() {
+			return this._startingTask;
+		}
 	});
 });
